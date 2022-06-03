a6b7b663a3615155ba99815560f7ce50dcf0d1a6
Jongmin Jack Kim <jmjkim00@icloud.com>
2022-06-02 18:18:56 -0700

    Adjust configuration

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: nom start
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+import { create, router as _router, defaults } from 'json-server';
+const server = create();
+const router = _router('db.json');
+const middlewares = defaults();
 const port = process.env.PORT || 5000;
 
 server.use(middlewares);
